deb2110d9234066051661e9509ff02d166adcc19
Tijunel <justintijunel@gmail.com>
2020-10-19 18:55:38 -0600

    Updates

---
+++ precache-manifest.8674ef16dfe792ca75fdb54bb4bdffdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e635da9668e562e11c01e0a95b6ba8",
+    "url": "/mine-sweep/index.html"
+  },
+  {
+    "revision": "c4fcb13e463b2b7e7fe1",
+    "url": "/mine-sweep/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a3b862cc88cf2f912e6c",
+    "url": "/mine-sweep/static/css/main.293e3d25.chunk.css"
+  },
+  {
+    "revision": "c4fcb13e463b2b7e7fe1",
+    "url": "/mine-sweep/static/js/2.12bb69b2.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/mine-sweep/static/js/2.12bb69b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b862cc88cf2f912e6c",
+    "url": "/mine-sweep/static/js/main.69bc3a3d.chunk.js"
+  },
+  {
+    "revision": "487b5b2aa1d14a0326fc",
+    "url": "/mine-sweep/static/js/runtime-main.f571e164.js"
+  }
+]);
